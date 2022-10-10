c836a0160eca6031eac7c199fd06ac76cbdabca3
caoguiming <1677664380@qq.com>
2022-10-10 15:45:17 +0800

    【i】【CmcOrg】

--- mall-start/src/main/java/com/cmcorg/service/mall/start/MallStartApplication.java
+++ mall-start/src/main/java/com/cmcorg/service/mall/start/MallStartApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class StartApplication {
+public class MallStartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StartApplication.class, args);
+        SpringApplication.run(MallStartApplication.class, args);
     }
 
 }
