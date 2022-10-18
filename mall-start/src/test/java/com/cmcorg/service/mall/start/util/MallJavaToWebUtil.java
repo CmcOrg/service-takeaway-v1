92a512292c79a78152e009e485db13c7f5ae75e2
caoguiming <1677664380@qq.com>
2022-10-18 18:39:00 +0800

    【i】【CmcOrg】

--- mall-start/src/test/java/com/cmcorg/service/mall/start/util/MallJavaToWebUtil.java
+++ mall-start/src/test/java/com/cmcorg/service/mall/start/util/MallJavaToWebUtil.java
@@ -5,7 +5,7 @@ import com.cmcorg.generate.page.h5.util.WebJsonToWebUtil;
 public class MallJavaToWebUtil {
 
     public static void main(String[] args) {
-        WebJsonToWebUtil.exec("D:/GitHub/CmcOrg/frontend-h5-mall-v1/"); // 生成前端页面
+        WebJsonToWebUtil.exec("E:/GitHub/CmcOrg/frontend-h5-mall-v1/"); // 生成前端页面
     }
 
 }
