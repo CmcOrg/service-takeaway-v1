da0f077170b77ffff37cbdbdf864ecfb74096b17
法外狂徒李四 <dimensional_logic@qq.com>
2022-11-05 12:15:27 +0800

    【i】【CmcOrg】

--- takeaway-start/src/test/java/com/cmcorg/service/takeaway/start/util/TakeawayJavaToWebUtil.java
+++ takeaway-start/src/test/java/com/cmcorg/service/takeaway/start/util/TakeawayJavaToWebUtil.java
@@ -5,7 +5,7 @@ import com.cmcorg.generate.page.h5.util.WebJsonToWebUtil;
 public class TakeawayJavaToWebUtil {
 
     public static void main(String[] args) {
-        WebJsonToWebUtil.exec("E:/GitHub/CmcOrg/frontend-h5-mall-v1/"); // 生成前端页面
+        WebJsonToWebUtil.exec("D:/GitHub/CmcOrg/frontend-h5-takeaway-v1/"); // 生成前端页面
     }
 
 }
