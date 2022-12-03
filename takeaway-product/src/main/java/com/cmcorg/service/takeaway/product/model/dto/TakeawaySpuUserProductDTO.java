c0e81801010d68df36d8b93eb51a56d533f26098
法外狂徒李四 <dimensional_logic@qq.com>
2022-12-03 12:38:05 +0800

    【i】【CmcOrg】

--- takeaway-product/src/main/java/com/cmcorg/service/takeaway/product/model/dto/TakeawaySpuUserProductDTO.java
+++ takeaway-product/src/main/java/com/cmcorg/service/takeaway/product/model/dto/TakeawaySpuUserProductDTO.java
@@ -3,10 +3,14 @@ package com.cmcorg.service.takeaway.product.model.dto;
 import com.cmcorg.engine.web.model.generate.model.annotation.RequestField;
 import com.cmcorg.service.takeaway.product.model.enums.TakeawaySceneEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TakeawaySpuUserProductDTO {
 
     @NotNull
